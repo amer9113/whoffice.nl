e81a22afa7b5b85ec1747054e7feeaf2a79c86c4
AbdAlrzzaq Alommar <rn_mad@hotmail.com>
2018-12-24 01:32:06 +0200

    remove old queries from db update.

--- application/controllers/Database.php
+++ application/controllers/Database.php
@@ -16,11 +16,11 @@ class Database extends CI_Controller {
 		echo "Updating DB....<br>";
 		ob_end_flush();
 		flush();
-		$this->db->query('ALTER TABLE `students` DROP `website`, DROP `bio`');
+		/*$this->db->query('ALTER TABLE `students` DROP `website`, DROP `bio`');
 		$this->db->query("ALTER TABLE `card_1` ADD `which_work_is_selected` INT NOT NULL AFTER `work_3`");
 		$this->db->query("ALTER TABLE `card_3` ADD `properties_7` VARCHAR(255) NOT NULL AFTER `have_properties_2`, ADD `have_properties_7` TINYINT NOT NULL AFTER `properties_7`");
 		$this->db->query("ALTER TABLE `card_5` ADD `days_work` TINYINT NOT NULL AFTER `training_multi_year`, ADD `days_school` TINYINT NOT NULL AFTER `days_work`");
-		$this->db->query("ALTER TABLE `card_6` ADD `parents` TINYINT NOT NULL AFTER `sports_club_friend`, ADD `another_parents` TINYINT NOT NULL AFTER `parents`");
+		$this->db->query("ALTER TABLE `card_6` ADD `parents` TINYINT NOT NULL AFTER `sports_club_friend`, ADD `another_parents` TINYINT NOT NULL AFTER `parents`");*/
 		$this->db->query("ALTER TABLE `students` CHANGE `username` `username` VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL");
 		$this->db->query("ALTER TABLE `students` CHANGE `firstname` `firstname` VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL");
 		$this->db->query("ALTER TABLE `students` CHANGE `lastname` `lastname` VARCHAR(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL");
