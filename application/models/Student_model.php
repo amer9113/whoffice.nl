b7d553b8f9515ae4fe331494ea599bba84c287b8
AbdAlrzzaq Alommar <rn_mad@hotmail.com>
2018-12-28 23:01:41 +0200

    System sends email to student time is done.

--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -2,6 +2,55 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student_model extends CI_Model{
+
+	public function check_email_host($email) {
+		if (trim($email) == "") {
+			return false;
+		}
+	    $domain_name = substr($email,(strpos($email, '@'))+1);
+	    if ( checkdnsrr($domain_name, 'ANY') ) {
+		  	return true;
+		}
+		else {
+		  	return false;
+		}
+	}
+
+	public function send_email($email,$msg,$subject) {
+		$this->config->load('email');
+		$smtp_host = $this->config->item('smtp_host');
+		$smtp_user = $this->config->item('smtp_user');
+		$smtp_pass = $this->config->item('smtp_pass');
+
+		if (!empty($smtp_host) && !empty($smtp_user) && !empty($smtp_pass)) {
+			if ($this->check_email_host($email)) {
+				$this->email->from($smtp_user);
+				$this->email->to($email);
+				$this->email->subject($subject);
+				$this->email->message($msg);
+
+				$result = false;
+				try {
+				   set_error_handler(create_function('', "throw new Exception(); return true;")); 
+				   $result = $this->email->send();
+				} catch(Exception $e) { 
+				   
+				}
+
+
+				if (!$result){
+					return "not_sent";
+				}else{
+					return "sent";
+				}
+			}else{
+				return "student email isn't valid.";
+			}
+		}else{
+			return "missing email configs.";
+		}
+	}
+
 	public function get_last_approved_card_no(){
 		for ($i = 8; $i > 0 ; $i--) { 
 			$query = $this->db->where('user_id',$this->acc_id)->where('checked_with_teacher',1)->get("card_$i")->num_rows();
@@ -51,6 +100,34 @@ class Student_model extends CI_Model{
 		->get('student_visites');
 
 		if ($query->num_rows() > 0) {
+
+			if ($query->row()->time_elapsed >= 64*60*60) {
+				
+				$student = $this->db->where('id',$this->acc_id)->get('students')->row();
+
+				if ($student->sent_completion_time_email == 0) {
+					$msg = '<html lang="NL">
+					    <head>
+					    <title></title>
+					    </head>
+					    <body>
+					        <p>Hello <b>'.$student->firstname.' '.$student->lastname.'</b></p>
+					        <p>You have completed your 64 hours on whoffic.</p>
+					        <p>Thank you.</p>
+					        <p>Support Team.</p>
+					    </body>
+					</html>';
+
+					$subject = "Whoffice completion time.";
+
+					$response = $this->send_email($student->email,$msg,$subject);
+
+					if ($response == "sent") {
+						$this->db->where('id',$this->acc_id)->set('sent_completion_time_email',1)->update('students');
+					}
+
+				}
+			}
 			return formatSecondsForHours($query->row()->time_elapsed)."H(s)";
 		}else{
 			return 0;
