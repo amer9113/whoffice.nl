fbb15189c1ccd1885455e168ffb80a228f714c9b
amer_naser <amer9113@gmail.com>
2018-12-31 19:05:13 +0100

    text netherland

--- application/views/student/student_card_response_messages.inc.php
+++ application/views/student/student_card_response_messages.inc.php
@@ -4,21 +4,21 @@
             <?php if ($data->checked_with_teacher == 0): ?>
                 <?php if ($data->needs_correction_by_student == 1): ?>
                     <div class="alert alert-danger text-center" role="alert">
-                        <strong>Card Needs Correction</strong><br><strong>Teacher Notes: </strong><?= $data->correction_notes; ?>
+                        <strong>Kaart moet worden gecorrigeerd</strong><br><strong>Opmerking van de docent: </strong><?= $data->correction_notes; ?>
                     </div>
                 <?php else: ?>
                     <div class="alert alert-danger text-center" role="alert"><strong>Waiting</strong> for teacher approval to go to the next card.</div>
                     <?php if ($data->correction_notes != ""): ?>
                         <div class="alert alert-info text-center" role="alert">
-                            <strong>Teacher Notes: </strong><?= $data->correction_notes; ?>
+                            <strong>Opmerking van de docent: </strong><?= $data->correction_notes; ?>
                         </div>
                     <?php endif ?>
                 <?php endif ?>
                 
             <?php else: ?>
                 <div class="alert alert-success text-center" role="alert">
-                    <strong>Well done!</strong> <?php if ($card_number < 8): ?>
-                        You can now take <a href="<?= base_url().'student/lesson/'.($card_number+1); ?>" class="alert-link">card<?= $card_number+1; ?></a>.
+                    <strong>Goed gedaan!</strong> <?php if ($card_number < 8): ?>
+                        U kunt nu <a href="<?= base_url().'student/lesson/'.($card_number+1); ?>" class="alert-link">kaart <?= $card_number+1; ?></a> pakken.
                     <?php endif ?>
                 </div>
             <?php endif ?>
