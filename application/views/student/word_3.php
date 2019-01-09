c6a1b186d9bb96213ba3857f3b804955a3c235fb
AbdAlrzzaq Alommar <rn_mad@hotmail.com>
2019-01-09 17:51:04 +0200

    Add word3.php.

---
+++ application/views/student/word_3.php
@@ -0,0 +1,219 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="NL">
+<head>
+	<?php require(realpath(dirname(__FILE__) . '/..') . '/inc/head.php') ?>
+	<title>Ultlegkaart3</title>
+</head>
+<body>
+    <div class="parallax">
+    <div class="mheader w3-container w3-mobile w3-card-4">
+        <img class="imgtext" src="<?= base_url().'images/header.png'; ?>">
+    </div>
+    <div class="cnt_heads w3-container w3-mobile">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8 w3-container hollcnt w3-container w3-mobile we-border w3-card-4">
+        <div class="w3-mobile">
+        <h2 class="cnt_heads w3-black">Resultaatkaart 3 – Je eigenschappen kennen; wie ben ik?</h2>
+        <label>In een vacature staan altijd de competenties (eigenschappen) genoemd die je nodig hebt om een baan goed te kunnen doen. Tijdens een sollicitatiegesprek wordt ook altijd gevraagd welke eigenschappen je hebt; je moet vertellen waar je goed in bent maar ook waar je niet zo goed in bent.
+        </label>
+        <br>
+        <label>Hieronder staan eigenschappen die mogelijk voor jou gelden;</label>
+        <br>
+        </div>
+        <div class="w3-mobile">
+        <h4 class="cnt_heads w3-red">3.1 Opdracht: zet een cirkel om de eigenschappen die jij hebt.</h4>
+        <ul>
+            <li><p>Accuraat zijn; dit betekent dat je heel precies en netjes werkt en geen fouten wilt maken</p></li>
+            <li><p>Afhankelijk zijn: je kunt niet zelfstandig werken en bent afhankelijk van wat een ander van je wil; je loopt het liefste achter iemand anders aan</p></li>
+            <li><p> Anticiperen:  dit is vooruitzien. Als je kunt anticiperen dan heb je een goede kijk op bepaalde ontwikkelingen. Dat kunnen ontwikkelingen zijn buiten de organisatie maar ook in de eigen organisatie of in je eigen leven. Je voorziet iets en neemt stappen of maatregelen.</p></li>
+            <li><p> Afspraak is afspraak: past dat bij jou? Dit is een kwaliteit maar ook een kernwaarde.  Dat betekent dat men van jou op aankan. Iemand die betrouwbaar is, zal gemaakte afspraken in principe altijd nakomen. Als afspraken toch niet nagekomen kunnen worden dat vertel je waarom niet.</p></li>
+            <li><p> Ambitieus zijn; vanuit jezelf werk je heel  hard om steeds beter te worden in wat je doet</p></li>
+            <li><p> Behulpzaam zijn of hulpvaardig.  Met deze eigenschap ben je gericht op de ander; je bent aardig en attent en aangenaam in de omgang met andere mensen; mensen zijn graag in jouw omgeving.     </p></li>
+            <li><p> Collegiaal zijn; collegialiteit: wie collegiaal is kijkt niet alleen naar zijn eigen werk maar kijkt ook naar de taken en werkzaamheden van andere collega’s . Er is bereidheid om te overleggen, bij te dragen met ideeën en om mee te denken met collega’s.</p></li>
+            <li><p> Communicatief; er zijn twee soorten:<br>
+            Mondeling communicatief zijn is: je kunt aan anderen goed uitleggen wat je belangrijk vind en je durft ook vragen te stellen; je kunt ook goed naar anderen luisteren; dat is belangrijk om echt te begrijpen wat de ander wil of belangrijk vindt.<br>
+            Schriftelijke communicatievaardigheden: soms heb je contact met iemand via een brief of mail; je moet dan bijvoorbeeld goed op papier kunnen zetten waar het over gaat en wat de afspraken zijn. Je moet goede brieven en mails kunnen schrijven.
+            </p></li>
+            <li><p> Discipline: gedisciplineerd zijn; je houd je aan regels die nodig zijn; de regels van het bedrijf of van jouw eigen bedrijf; dit doe je ook als dat lastig voor je is.</p></li>
+            <li><p> Doorzettingsvermogen: je bent een doorzetter; je blijft doorgaan met je werk, zelfs als je dat niet leuk vindt of als je het moeilijk vindt; je geeft niet op.</p></li>
+            <li><p> Enthousiast: je voelt je heel blij met wat je doet en laat dat ook aan andere mensen zien.</p></li>
+            <li><p> Flexibel: je kunt je makkelijk aanpassen; je beweegt mee met de ontwikkelingen. Als een situatie ineens anders gaat dan pas jij je daaraan aan. </p></li>
+            <li><p> Initiatief nemen: je zoekt actief naar dingen die je verder helpen; je zoekt bijvoorbeeld zelf al naar werk en laat jouw verbeteringen niet afhangen van andere mensen.</p></li>
+            <li><p> Klantgericht zijn: je luistert goed naar wat de klant zegt en probeert daarna een oplossing te vinden; je wilt dat de klant tevreden is.</p></li>
+            <li><p> Leidinggeven: je vind het leuk om mensen aan te sturen; het maakt je blij als door jouw inzet als leidinggevende het bedrijf beter wordt en mooie resultaten worden bereikt. Je kunt goed communiceren en hebt oog voor de mensen waarmee je werkt; je maakt gebruik van hun talenten.</p></li>
+            <li><p> Motivatie: gemotiveerd zijn. Je bent heel erg enthousiast over iets dat je doet en laat dat ook aan anderen zien.</p></li>
+            <li><p> Onafhankelijk: je denkt zelf na en je doet niet automatisch iets wat anderen ook doen. Je organiseert zelf je eigen werk.</p></li>
+            <li><p> Prioriteiten stellen; kiezen bij veel werk wat het belangrijkste is om het eerste mee te beginnen.</p></li>
+            <li><p> Pro actief zijn: als je een probleem ziet ga je meteen kijken naar een oplossing.</p></li>
+            <li><p> Representatief zijn: je bent een visitekaartje voor jouw bedrijf. Je bent netjes en verzorgd gekleed en behandelt klanten met aandacht en respect.</p></li>
+            <li><p> Samenwerken: je werkt liever met anderen samen dan dat je alles alleen moet doen.</p></li>
+            <li><p> Speels: je bent fantasierijk en je hoofd zit vol met ideeën; je kunt speels zijn met taal, woorden of juist met getallen en cijfers; het ligt er aan waar je interesse ligt.</p></li>
+            <li><p> Stressbestendig: Als er heel veel tegelijk gebeurt kun je daar heel zenuwachtig (gestrest) van worden. Als je ook in heftige situaties rustig kan blijven ben je stressbestendig.</p></li>
+            <li><p> Verantwoordelijkheidsgevoel: wie zich verantwoordelijk gedraagt doet geen ondoordachte dingen maar is serieus en maakt keuzes op basis van logische redeneringen.</p></li>
+            <li><p>Zelfstandig: je kunt zelfstandig werken en vraagt een ander niet steeds om te helpen. Als je net pas met je baan bent begonnen kun je dat natuurlijk wel doen.</p></li>
+            <li><p>Zorgvuldig: je kunt bijvoorbeeld nauwkeurig en precies zijn; dat wil zeggen dat je oof hebt voor details (kleine dingen). Je kunt ook zorgvuldig zijn in je afwegingen (de basis waarop  je een beslissing neemt); dat betekent dat je best je mening wilt geven maar niet nadat je deze zorgvuldig hebt onderbouwt (aantonen of bewijzen) en beargumenteerd (er woorden aan hebt gegeven; uitleggen waarom je de beslissing hebt genomen.</p></li>
+        </ul>
+        <br>
+        <br>
+        </div>
+        <div class="w3-mobile">
+        <h4 class="cnt_heads w3-red">3.2 Opdracht </h4>
+        <p>Je hebt jouw eigenschappen omcirkelt; schrijf ze hieronder op:</p>
+        <p>…………………………………………………………………………………………………………………………………………………………………………………………………</p><p>…………………………………………………………………………………………………………………………………………………………………………………………………</p><p>…………………………………………………………………………………………………………………………………………………………………………………………………</p><p>…………………………………………………………………………………………………………………………………………………………………………………………………</p>
+        
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">3.3 Opdracht </h4>
+            <p>Teken op een blaadje je eigen levenslijn vanaf je geboorte tot aan je huidige leeftijd; de twee jaartallen die daarbij horen schrijf je aan het begin en het einde van je levenslijn.</p>
+            <p>Welke belangrijke gebeurtenissen zijn er in jouw leven gebeurd? Schrijf ze samen met de leeftijd waarop dat gebeurde op in je levenslijn. Doe dat in het kort met een of twee woorden.</p>
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">3.4 Opdracht</h4>
+            <p>Kijk nu in de competentielijst hierboven en zoek welke eigenschappen je tijdens je leven hebt kunnen gebruiken om te komen waar je nu bent. Schrijf deze hieronder op.</p>
+            <table style="width: 100%">
+                <tr>
+                    <td>1.…………………………………………….</td>
+                    <td>2.…………………………………………….</td>
+                    <td>3. …………………………………………….</td>
+                </tr>
+                <tr>
+                    <td>4.…………………………………………….</td>
+                    <td>5.…………………………………………….</td>
+                    <td>6.…………………………………………….</td>
+                </tr>
+                <tr>
+                    <td>7.…………………………………………….</td>
+                    <td>8.…………………………………………….</td>
+                    <td>9.…………………………………………….</td>
+                </tr>
+                <tr>
+                    <td>10.…………………………………………….</td>
+                    <td>11.…………………………………………….</td>
+                    <td>12.…………………………………………….</td>
+                </tr>
+                <tr>
+                    <td>13.…………………………………………….</td>
+                    <td>14.…………………………………………….</td>
+                    <td>15.…………………………………………….</td>
+                </tr>
+            </table>
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">3.5 Opdracht </h4>
+            <label>Wat is je wensberoep? …………………………………………………………………………………………..</label>
+            <p>Zoek dit beroep of een vacature hiervan op internet, bijvoorbeeld op </p><a href="www.werk.nl">www.werk.nl</a>
+            <p>of zoek een advertentie in de krant of bij het uwv<a href="www.uwv.nl">www.uwv.nl</a>Als je deze gevonden </p>
+            <p>hebt kijk je naar de competenties die worden gevraagd en schrijf je ze hieronder op </p>
+            <p>…………………………………………………………………………………………………………………………………………………………………………………………………</p><p>…………………………………………………………………………………………………………………………………………………………………………………………………</p><p>…………………………………………………………………………………………………………………………………………………………………………………………………</p><p>…………………………………………………………………………………………………………………………………………………………………………………………………</p>
+            <div class="w3-pannel">
+                <label>3.6 Praten over je competenties</label>
+                <p>Je bent niet gewend om te praten over je eigen kwaliteiten/competenties; soms weet je ook niet waar je echt goed in bent. </p>
+                <p>Bij de oefeningen van deze les heb je al gekeken waar je “sterk” in bent. Als je gaat solliciteren wordt hier altijd naar gevraagd. Je maakt het voor jezelf makkelijker als je hier in deze les al een paar voorbeelden maakt zodat je die bij een </p>
+                <p>kennismakingsgesprek kunt gebruiken.</p>
+                <p>Er bestaat een methode die de “Starmethode” heet; psychologen hebben die gemaakt om mensen te helpen om over je goede eigenschappen te praten.</p>
+                <p>De betekenis van STAR is:</p>
+                <p>S &nbsp; in welke Situatie heb ik mijn eigenschap laten zien?</p>
+                <p>T &nbsp; wat was jouw Taak?</p>
+                <p>A &nbsp; hoe heb je het Aangepakt?</p>
+                <p>R &nbsp; wat was het Resultaat?</p>
+                <br>
+                <p>* Situatie: in mijn straat woont een echtpaar met kinderen waarvan de man in het ziekenhuis ligt.</p>
+                <p>* Taak: bij de kringloopwinkel hebben zij een wasmachine kunnen kopen en die moet worden opgehaald, naar de eerste etage gebracht en worden aangesloten.</p>
+                <p>* Aanpak: ik heb een vriend die een auto met trekhaak heeft en heb hem gevraagd mij te helpen met het ophalen en naar boven brengen van de wasmachine.</p>
+                <p>Wij hebben samen de machine opgehaald en naar boven gebracht; daarna heb ik de machine aangesloten.</p>
+                <p>* Resultaat: een hele blije familie die meteen de eerste was in de machine kon stoppen. Haar man was ook heel blij en hoefde zich in het ziekenhuis geen zorgen meer te maken.</p>
+            <br>
+            </div>
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">3.7 In een schema ziet dit er als volgt uit:</h4>
+            <table class="table table-bordered" style="width: 100%">
+                <tr>
+                    <td>Competentie</td>
+                    <td>Situatie, taak en aanpak   </td>
+                    <td>Resultaat</td>
+                </tr>
+                <tr>
+                    <td>behulpzaam</td>
+                    <td>Man in ziekenhuis, vrouw en kinderen zonder<br>wasmachine; hulp zoeken voor <br>vervoer en plaatsing<br>van de machine; machine aansluiten </td>
+                    <td>Het probleem is<br>opgelost en het gezin kan <br>weer wassen.<br>Iedereen blij.</td>
+                </tr>
+            </table>
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">3.8 Opdracht</h4>
+                <label>Kies twee competenties die bij jou horen en zet erbij hoe of in welke situatie je dat kunt laten zien.</label>
+            <div class="w3-pannel">
+                <ol>
+                    <li>1. Ik ben <br>………………………………………………………………………………………………………………………………
+                        <ol>
+                        <li>1a Hoe heb ik dat laten zien? Noem de situatie<br>………………………………………………………………………………………………………………………………</li>
+                        <li>1b Wat was jouw taak; wat moest jij doen?<br>………………………………………………………………………………………………………………………………</li>
+                        <li>1c Hoe heb jij dat aangepakt?<br>………………………………………………………………………………………………………………………………</li>
+                        <li>1d Wat was het resultaat; was de uitkomst heel goed of moest je nog wat anders doen?<br>………………………………………………………………………………………………………………………………</li>
+                        </ol>
+                    </li>
+                    <li>2. Ik ben  <br>………………………………………………………………………………………………………………………………
+                        <ol>
+                        <li>2a Hoe heb ik dat laten zien? Noem de situatie<br>………………………………………………………………………………………………………………………………</li>
+                        <li>2b Wat was jouw taak; wat moest jij doen?<br>………………………………………………………………………………………………………………………………</li>
+                        <li>2c Hoe heb jij dat aangepakt?<br>………………………………………………………………………………………………………………………………</li>
+                        <li>2d Wat was het resultaat; was de uitkomst heel goed of moest je nog wat anders doen?</li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">3.9</h4>
+            <label>Hierboven heb je geleerd welke goede of sterke eigenschappen je bezit maar niemand heeft alleen maar sterke kanten; weet jij al wat je zwakke of zwakkere kanten zijn?</label>
+            <p>Wat is een zwak punt; dit is bijvoorbeeld iets wat je niet zo goed kunt. Je bent bijvoorbeeld niet zo goed met andere talen of je kunt niet met andere mensen  samenwerken.</p>
+            <p>Als dit echt een zwak punt is, is het de vraag of je een beroep moet zoeken waar je die punten juist wel nodig hebt; je kunt beter zoeken naar het ontwikkelen van je sterke punten.</p>
+            <p>Als je weet wat er niet zo goed gaat kun je dat misschien veranderen, Hieronder volgen wat punten van dingen die niet goed kunnen gaan:</p>
+            <ul>
+                <li><p>•    Altijd aardig en leuk gevonden willen worden; altijd aardig zijn en goed praten over mensen die akelige dingen over je zeggen; je durft niet te zeggen wat je denkt of voelt omdat je bang bent dat ze je anders niet meer aardig vinden.
+                Wie te aardig is, is eigenlijk onaardig. Te aardig zijn is iets waar jezelf niets aan hebt en waar je anderen geen plezier mee doet. Wie te aardig is, is onduidelijk over zijn eigen grenzen en dus ook over eigen mogelijkheden. Anderen weten niet altijd wat ze aan je hebben, Wil je dat nu echt? Of zeg je maar gewoon dat je het wilt? Bovendien krijg je van anderen niet het respect wat je eigenlijk wel zou willen ontvangen.</p></li>
+                <li><p>•    Jezelf wegcijferen: je geeft voorrang aan wat een ander nodig heeft en geeft te weinig aandacht aan wat jezelf nodig hebt. Je kijkt tegen mensen op of je vind jezelf niet goed genoeg; je vind de wensen en belangen van andere belangrijker dan je eigen belangen.<br>Wat kun je doen? Stoppen?<br>Herken je dit gedrag? Op je werk, in de relatie met je partner of met je kinderen? Dan nodigen wij je uit om daarmee te stoppen. Het brengt je niets en het heeft voor een ander geen toegevoegde waarde; uiteindelijk helpt het niet om een goede eerlijke relatie te houden.</p></li>
+            </ul>
+            <p>Hieronder volgen nog wat zwakke eigenschappen in losse woorden; kijk of er woorden bij zitten die op jou van toepassing zijn:</p>
+            <table style="width: 100%">
+                <tr>
+                    <td>1.  Ik praat veel</td>
+                    <td>2. Ik ben lui</td>
+                    <td>3. Ik ben langzaam</td>
+                </tr>
+                <tr>
+                    <td>4.  Ik ben slordig</td>
+                    <td>5. Ik kan slecht luisteren</td>
+                    <td>6. Ik ben jaloers</td>
+                </tr>
+                <tr>
+                    <td>7.  Ik voel mij onzeker</td>
+                    <td>8. Ik ben verlegen</td>
+                    <td>9. Ik kom altijd te laat</td>
+                </tr>
+                <tr>
+                    <td>10  Ik ben ongeduldig</td>
+                    <td>11 Ik ben verlegen</td>
+                    <td>12 Ik kan geen nee zeggen</td>
+                </tr>
+            </table>
+        </div>
+        <div class="w3-mobile">
+            <h4 class="cnt_heads w3-red">4.0 Opdracht</h4>
+            <label>Schrijf 3 dingen op die soms niet goed gaan</label>
+            <p>1 ……………………………………………………………………….</p>
+            <p>2………………………………………………………………………..</p>
+            <p>3………………………………………………………………………..</p>
+            <p>Je hebt in deze les heel veel nieuwe woorden geleerd en weet nu ook wat je sterke en zwakke kwaliteiten/competenties zijn; je kunt je 3e resultaatkaart nu vast heel goed invullen.</p>
+            <p>Als je gemerkt hebt dat je competenties mist om je wensberoep uit te kunnen oefen dan kun je daar nu mee gaan oefenen.</p>
+        </div>
+    </div>
+    </div>
+    </div>
+<?php require(realpath(dirname(__FILE__) . '/..') . '/inc/scripts.php') ?>
+</body>
+</html>
